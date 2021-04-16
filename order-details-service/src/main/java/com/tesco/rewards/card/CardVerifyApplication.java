99367973d7301a53ce55b1950755d4aa6e364241
sreenu dandu <sreenu.sreenutech@gmail.com>
2021-04-16 07:48:40 +0530

    main class changes

--- order-details-service/src/main/java/com/tesco/rewards/card/CardVerifyApplication.java
+++ order-details-service/src/main/java/com/tesco/rewards/card/CardVerifyApplication.java
@@ -1,9 +1,8 @@
 package com.tesco.rewards.card;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+;
 
-@SpringBootApplication
+
 public class CardVerifyApplication {
 
 	public static void main(String[] args) {

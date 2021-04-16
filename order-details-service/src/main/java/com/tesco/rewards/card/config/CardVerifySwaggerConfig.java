8d5f40576d1066ae3c57b32e9e032387bc567026
sreenu dandu <sreenu.sreenutech@gmail.com>
2021-04-16 07:30:29 +0530

    removed unused to code

--- order-details-service/src/main/java/com/tesco/rewards/card/config/CardVerifySwaggerConfig.java
+++ order-details-service/src/main/java/com/tesco/rewards/card/config/CardVerifySwaggerConfig.java
@@ -16,21 +16,5 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class CardVerifySwaggerConfig extends WebMvcConfigurationSupport {
-	@Bean
-	public Docket balanceApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage(SwaggerConstants.BASE_PACKAGE)).paths(PathSelectors.any())
-				.build();
-	}
-
-	/**
-	 * This method is used to generate swagger UI and need to provide the ui
-	 * resources location
-	 */
-	@Override
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(SwaggerConstants.FILENAME).addResourceLocations(SwaggerConstants.FILE_LOCATION);
-		registry.addResourceHandler(SwaggerConstants.RESOURCE_HANLDER)
-				.addResourceLocations(SwaggerConstants.RESOURCE_HANDLER_LOC);
-	}
+	
 }
